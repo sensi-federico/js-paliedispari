90ec044a98ee53059c354d2c998fd41b3250695a
sensiz <dev.federicosensi@gmail.com>
2022-10-14 12:10:15 +0200

    initial commit

---
+++ palindroma/assets/js/main.js
@@ -0,0 +1,12 @@
+
+
+// Palidroma
+// Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+
+
+
+// chiedo una stringa all'utente e la copio in una variabile
+// trasformo la stringa copiata in array con split()
+// inverto l'array con reverse()
+// riporto l'array in stringa con join()
+// confronto se le due variabili e stampo messaggio a schermo
