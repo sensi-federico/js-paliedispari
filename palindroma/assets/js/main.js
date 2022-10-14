ada59b06c9232204b16a3838fdfae4a6df879d99
sensiz <dev.federicosensi@gmail.com>
2022-10-14 12:35:59 +0200

    final commit

--- palindroma/assets/js/main.js
+++ palindroma/assets/js/main.js
@@ -4,9 +4,27 @@
 // Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
 
 
-
 // chiedo una stringa all'utente e la copio in una variabile
+const string = String(prompt('Inserisci una parola'));
+const stringCopy = string;
+
 // trasformo la stringa copiata in array con split()
+const stringToArray = stringCopy.split('');
+
 // inverto l'array con reverse()
+const stringReverse = stringToArray.reverse();
+
 // riporto l'array in stringa con join()
+const arrayToString = stringReverse.join('');
+// console.log(stringToArray);
+const reverse = arrayToString;
+
+console.log(string);
+console.log(reverse);
+
 // confronto se le due variabili e stampo messaggio a schermo
+if (string == reverse) {
+    console.log('La parola è palindroma');
+} else {
+    console.log('La parola non è palindroma');
+}
